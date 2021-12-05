87c616eb6886ef5831b4a27820de65132c1422d5
Brynne Eastman <brynne.eastman@gmail.com>
2021-12-05 14:16:33 -0500

    fixed errors in server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const hbs = exphbs.create({ helpers });
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
+const sequelize = require('./config/config.json');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 
