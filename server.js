b053782172b7823e74782dece1050c720e4fa890
Brynne Eastman <brynne.eastman@gmail.com>
2021-12-12 14:27:54 -0500

    linked games to dashboard, edited main.handlebars

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+const routes = require("./controllers");
+//const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
 
 const app = express();
